15d55a869351904b69bd0cc679bb4fbfa3fb2b97
StefanoR <rubinetti.stefano@gmail.com>
2025-03-31 10:56:54 +0200

    2

--- src/main/java/com/generation/primoprogettospring/controllers/PrimoController.java
+++ src/main/java/com/generation/primoprogettospring/controllers/PrimoController.java
@@ -1,8 +1,10 @@
 package com.generation.primoprogettospring.controllers;
 
+import com.generation.primoprogettospring.model.Persona;
 import com.generation.primoprogettospring.model.PersonaDAO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
@@ -23,8 +25,25 @@ public class PrimoController
 	}
 
 	@GetMapping("/es1")
-	public String es1()
+	public String es1(Model m)
 	{
 		return "es1";
+	}
+
+	@GetMapping("/esempio1")
+	public String esempio1(Model m)
+	{
+		//							usato con Optional per decidere cosa restituire se è vuoto
+		Persona p = dao.findById(1L).orElse(null);
+		//Optional<Persona>, un oggetto che può contenere o no una persona
+		//se trovi la persona restituiscila, altrimenti metti dentro p null
+		//posso passarla alla pagina web grazie al model
+		m.addAttribute("pers",p);//sto inserendo la persona
+		//all'interno del Model, che è una MAPPA
+		//la sua chiave è pers, il valore è l'oggetto di p
+		//pers sarà utilizzabile nella pagina web
+
+		//Il model contiene tutte le variabili passate alla pagina web
+		return "esempio1";
 	}
 }
--- src/main/java/com/generation/primoprogettospring/model/Persona.java
+++ src/main/java/com/generation/primoprogettospring/model/Persona.java
@@ -9,16 +9,75 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Entity
-@Data//getter setter tostring
-@AllArgsConstructor//costruttore pieno
-@NoArgsConstructor//costruttore vuoto
 public class Persona
 {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
-	private String nome,cognome;
+	private String nome,cognome,presentazione,indirizzo;
 
 
+	public Persona()
+	{
+	}
+
+	public Persona(Long id, String nome, String cognome, String presentazione, String indirizzo)
+	{
+		this.id = id;
+		this.nome = nome;
+		this.cognome = cognome;
+		this.presentazione = presentazione;
+		this.indirizzo = indirizzo;
+	}
+
+	public Long getId()
+	{
+		return id;
+	}
+
+	public void setId(Long id)
+	{
+		this.id = id;
+	}
+
+	public String getNome()
+	{
+		return nome;
+	}
+
+	public void setNome(String nome)
+	{
+		this.nome = nome;
+	}
+
+	public String getCognome()
+	{
+		return cognome;
+	}
+
+	public void setCognome(String cognome)
+	{
+		this.cognome = cognome;
+	}
+
+	public String getPresentazione()
+	{
+		return presentazione;
+	}
+
+	public void setPresentazione(String presentazione)
+	{
+		this.presentazione = presentazione;
+	}
+
+	public String getIndirizzo()
+	{
+		return indirizzo;
+	}
+
+	public void setIndirizzo(String indirizzo)
+	{
+		this.indirizzo = indirizzo;
+	}
 }
